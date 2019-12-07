0c923760020f4b99ac40b6829d55a4d901ca913c
nstilt1 <43357580+nstilt1@users.noreply.github.com>
2019-12-06 21:45:32 -0600

    Update Program.cs

--- KRK/Program.cs
+++ KRK/Program.cs
@@ -1,14 +1,16 @@
-﻿using System;
+using System;
 
 namespace KRK
 {
     class Program
     {
+        /**
+         * Runs the Calculator method.
+         **/
         static void Main(string[] args)
         {
             Calculator calc = new Calculator();
             calc.testStart();
-            //Console.WriteLine("reeee");
         }
     }
 }
